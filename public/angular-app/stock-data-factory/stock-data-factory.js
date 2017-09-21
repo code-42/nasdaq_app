40171457eca808fdab9d9867d1e4665b1c88c7eb
ED <edwd42@gmail.com>
2017-09-21 08:39:06 -0400

    Update stock-data-factory.js
    added the web page url where I learned about the yahoo finance api

--- public/angular-app/stock-data-factory/stock-data-factory.js
+++ public/angular-app/stock-data-factory/stock-data-factory.js
@@ -8,6 +8,7 @@ function stockDataFactory($http){
 
   function stockDisplay(symbol){
     console.log("1.get.stockDataFactory.stockDisplay(symbol) == " + symbol);
+    // bookmark:  http://lekkerlogic.com/2015/04/live-market-data-with-yahoo-finance-api-json-jquery/
     return $http.get('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + symbol + '%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=').then(complete).catch(failed);
   }
 
